dca27110829b0457d97e90c60a88cb9ae6bb18ce
sourabh pisal <pisal.sourabh@gmail.com>
2024-06-07 22:18:45 +0100

    remove setup.sh after install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+rm -rf dotfiles
 alias dotfiles="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
 git clone --bare git@github.com:sourabh-pisal/dotfiles.git $HOME/dotfiles
 dotfiles switch -f mainline
 dotfiles config --local status.showUntrackedFiles no
+rm setup.sh
