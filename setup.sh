1d5f5949546eb44e9c69cf85ba0b881de30d84db
sourabh pisal <pisal.sourabh@gmail.com>
2024-06-08 10:02:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 rm -rf dotfiles
-alias dotfiles="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME/sourabh"
+alias dotfiles="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
 git clone --bare git@github.com:sourabh-pisal/dotfiles.git $HOME/dotfiles
 dotfiles switch -f mainline
 dotfiles config --local status.showUntrackedFiles no
 rm setup.sh
+rm Readme.md
