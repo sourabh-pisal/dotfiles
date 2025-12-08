1717da6d524189fe9cadd67ec0ed927e2f0ca30e
Sourabh Vijay Pisal <pisal.sourabh@gmail.com>
2025-12-08 19:42:03 +0000

    Fix lsp's

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,42 +1,64 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			vim.lsp.config("eslint", {
-				settings = {
-					codeActionOnSave = {
-						enable = true,
-						mode = "all",
-					},
-					format = true,
-				},
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_next, {})
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, {})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-			vim.keymap.set("n", "<leader>gl", ":LspEslintFixAll<CR>", {})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      vim.lsp.config("lua_ls", {})
+      vim.lsp.enable("lua_ls")
+
+      vim.lsp.config("ts_ls", {})
+      vim.lsp.enable("ts_ls")
+
+      vim.lsp.config("eslint", {
+        settings = {
+          codeActionOnSave = {
+            enable = true,
+            mode = "all",
+          },
+          format = true,
+        },
+      })
+      vim.lsp.enable("eslint")
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_next, {})
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      vim.keymap.set("n", "<leader>gl", ":LspEslintFixAll<CR>", {})
+    end,
+  },
 }
