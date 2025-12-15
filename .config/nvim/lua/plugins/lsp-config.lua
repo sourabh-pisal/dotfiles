038b4006a2ea07ac317853d99c7d2f7fe2576207
Sourabh Pisal <pisal.sourabh@gmail.com>
2025-12-15 18:14:29 +0000

    Add rust_analyzer lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -33,6 +33,7 @@ return {
     config = function()
       vim.lsp.enable("lua_ls")
       vim.lsp.enable("ts_ls")
+      vim.lsp.enable("rust_analyzer")
 
       vim.lsp.config("eslint", {
         settings = {
