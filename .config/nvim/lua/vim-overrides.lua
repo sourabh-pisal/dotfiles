1d31873340553c69756e50836f50c47fd4abc7a7
Sourabh Vijay Pisal <pisal.sourabh@gmail.com>
2026-03-03 21:41:52 +0000

    Add line wraps neovim

--- .config/nvim/lua/vim-overrides.lua
+++ .config/nvim/lua/vim-overrides.lua
@@ -9,7 +9,10 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "↪ "
 
 vim.opt.autoread = true
 
