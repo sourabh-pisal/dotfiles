97990bafe99256294946198df641436daff38156
Sourabh Pisal <pisal.sourabh@gmail.com>
2026-03-14 23:13:01 +0000

    Display home instead of ~ bash prompt

--- .config/bash/core/theme.bash
+++ .config/bash/core/theme.bash
@@ -10,12 +10,20 @@ __git_info() {
     echo -e " \e[34mgit:(\e[31m${branch}\e[34m)\e[0m${dirty}"
 }
 
+__dir_display() {
+    if [[ "$PWD" == "$HOME" ]]; then
+        echo "home"
+    else
+        echo "${PWD##*/}"
+    fi
+}
+
 build_prompt() {
     local EXIT=$?
     local ARROW_COLOR="\e[32m"
     [ $EXIT -ne 0 ] && ARROW_COLOR="\e[31m"
 
-    PS1="\[\e[33m\]\u\[\e[0m\]@\[\e[35m\]\h\[\e[0m\] \[${ARROW_COLOR}\]➜ \[\e[36m\]\W\[\e[0m\]$(__git_info)\[\e[0m\] "
+    PS1="\[\e[33m\]\u\[\e[0m\]@\[\e[35m\]\h\[\e[0m\] \[${ARROW_COLOR}\]➜ \[\e[36m\]$(__dir_display)\[\e[0m\]$(__git_info)\[\e[0m\] "
 }
 
 PROMPT_COMMAND=build_prompt
